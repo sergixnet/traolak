fa41bd8dcfc65f2c7ed4745d4fdc630181b11774
Sergio Peña <sergiop79@gmail.com>
2020-08-10 15:53:23 +0200

    Added social meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,9 @@ import Footer from './Footer';
 import Navigation from './Navigation';
 
 export default function Layout(props) {
-  const title = props.title ? props.title : `Azken ${props.hours} orduetako euskal joerak`;
+  const title = props.title
+    ? props.title
+    : `Azken ${props.hours} orduetako euskal joerak`;
   const description = props.description
     ? props.description
     : `Azken ${props.hours} orduetako euskal joerak twitter-en`;
@@ -12,9 +14,35 @@ export default function Layout(props) {
     <div className="site">
       <Head>
         <link rel="icon" href="/favicon.png" />
-        <link rel="canonical" href={`https://traolak.vercel.app${props.path}`}/>
-        <meta name="description" content={description} />
+        <link
+          rel="canonical"
+          href={`https://traolak.vercel.app${props.path}`}
+        />
+
         <title>{title} | #Traolak</title>
+        <meta
+          name="title"
+          content={title}
+        />
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`https://traolak.vercel.app${props.path}`} />
+        <meta
+          property="og:title"
+          content={title}
+        />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="https://traolak.vercel.app/traolak-social-image.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={`https://traolak.vercel.app${props.path}`} />
+        <meta
+          property="twitter:title"
+          content={title}
+        />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content="https://traolak.vercel.app/traolak-social-image.png" />
       </Head>
 
       <Header hours={props.hours} title="Twitter-eko joerak" />
