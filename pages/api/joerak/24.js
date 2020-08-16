3b964de7e363fed47453d284e4b998ffa71fff0d
Sergio Peña <sergiop79@gmail.com>
2020-08-16 18:39:46 +0200

    Implementing some cache to the api endpoints

--- pages/api/joerak/24.js
+++ pages/api/joerak/24.js
@@ -1,9 +1,22 @@
 import getJoerak from '../../../lib/scrapping';
+import cacheConfig from '../../../lib/cache-config';
+
+let cacheTime = new Date();
+const { maxTime } = cacheConfig;
+let cacheContent;
 
 export default async (req, res) => {
+  const now = new Date();
   if (req.method === 'GET') {
+    if (now - cacheTime < maxTime && cacheContent) {
+      return res
+        .status(200)
+        .json({ ...cacheContent, cache_time: cacheTime.getTime() });
+    }
     const joerak = await getJoerak(24);
-    res.status(200).json(joerak);
+    res.status(200).json({ ...joerak, cache_time: cacheTime.getTime() });
+    cacheContent = joerak;
+    cacheTime = new Date();
   } else {
     res.status(405).json({ message: 'Only GET method allowed' });
   }
